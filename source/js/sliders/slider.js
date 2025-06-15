8672dd615bca2b136d0073d2639c5b1a2c2b73d6
ToryaToria <toria.torya@yandex.ru>
2025-06-15 09:36:38 +0300

    разбираюсь со свайпером-грид

--- source/js/sliders/slider.js
+++ source/js/sliders/slider.js
@@ -1,24 +1,25 @@
 import Swiper from 'swiper';
-import { Pagination, Grid } from 'swiper/modules';
+import { Grid } from 'swiper/modules';
 
-import 'swiper/css/pagination';
-
+// import 'swiper/css/pagination';
+import 'swiper/css';
+import 'swiper/css/grid';
 
 const initSwiper = () => {
 
   const swiper = new Swiper('.mySwiper', {
-    modules: [ Pagination, Grid],
+    modules: [ Grid],
     allowTouchMove: true, // свайп и мышка
     autoHeight: false,
-    slidesPerView: 1,
-    // grid: {
-    //   rows: 2,
-    //   fill: 'column',  
-    // },
-    loop: false,
+    slidesPerView: 2,
+    grid: {
+      rows: 2,
+      fill: 'column',  
+    },
+    // loop: false,
     // updateOnWindowResize: true,
 
-    slidesPerColumn: 2,
+    // slidesPerColumn: 2,
     // slidesPerGroup :2,
     // slidesPerColumnFill: 'row',
 
