e825eb10fc9c758829f9d53ca417c2ff1ed70912
ToryaToria <toria.torya@yandex.ru>
2025-06-15 09:10:10 +0300

    ошибка +- в аккордеоне

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -2,12 +2,15 @@ const questions = document.querySelectorAll('.faq__item');
 const answers = document.querySelectorAll('.faq__answer');
 const titleQvestions = document.querySelectorAll('.faq__question');
 
+const byttonIcons = document.querySelectorAll('.button-plus');
+
 questions.forEach((elem, index) => {
   elem.addEventListener('click', () => {
     elem.classList.toggle('faq__item--open');
     answers[index].classList.toggle('faq__answer--open');
     titleQvestions[index].classList.toggle('faq__question--open');
 
+    byttonIcons[index].classList.toggle('button-plus--open');
   });
-
 });
+ 
