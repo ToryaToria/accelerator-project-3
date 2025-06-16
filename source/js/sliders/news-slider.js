aaf43e9e884496997fccb8073a256e277f78231f
ToryaToria <toria.torya@yandex.ru>
2025-06-16 09:03:05 +0300

    слайдер news - что успела

--- source/js/sliders/news-slider.js
+++ source/js/sliders/news-slider.js
@@ -1,13 +1,9 @@
-// https://swiperjs.com/get-started#installation
 import Swiper from 'swiper';
 import { Navigation, Scrollbar, Pagination, Grid } from 'swiper/modules';
-import 'swiper/css';
 import 'swiper/css/grid';
 
-console.log('news');
-
 const initSwiperNews = () => {
-  const swiperNews = new Swiper('.news__slider', {
+new Swiper('.news__slider', {
     modules: [Navigation, Scrollbar, Pagination, Grid],
     loop: false, // незациклен
     allowTouchMove: true, // свайп и мышка
@@ -21,39 +17,63 @@ const initSwiperNews = () => {
       fill: 'column',
     },
 
-    navigation: {
-      nextEl: '.swiper-button-next-news',
-      prevEl: '.swiper-button-back-news',
-    },
+    on: {
+      init: function () {
+        this.slides.forEach((slide, index) => {
+          if (index % 2 === 0) {
+            slide.style.height = '330px';
+          } else {
+            slide.style.height = '240px';
+          }
+        });
 
-    breakpoints: {
-      768: {
-        spaceBetween: 30,
-        slidesPerView: 2,
+        if (document.documentElement.clientWidth >= 768) {
+          this.slides.forEach((slide) => {
+            slide.style.height = '350px';
+          });
+        }
 
-        grid: {
-          rows: 2,
-          fill: 'row',
-        },
+        if (document.documentElement.clientWidth >= 1440) {
+          this.slides.forEach((slide) => {
+            slide.style.height = '400px';
+            slide.style.width = '286px';
+            if (slide.classList.contains('swiper-slide-active')) {
+              slide.style.width = '604px';
+            }
+          });
+        }
 
       },
-      1440: {
-        spaceBetween: 32,
-        slidesPerView: 'auto',
 
-        grid: {
-          rows: 1,
-          fill: 'colomn',
-        },
+      navigation: {
+        nextEl: '.swiper-button-next-news',
+        prevEl: '.swiper-button-back-news',
       },
+
+      breakpoints: {
+        768: {
+          spaceBetween: 30,
+          slidesPerView: 2,
+
+          grid: {
+            rows: 2,
+            fill: 'row',
+          },
+
+        },
+        1440: {
+          spaceBetween: 32,
+          slidesPerView: '3',
+
+          grid: {
+            rows: 1,
+            fill: 'colomn',
+          },
+        },
+      }
+
     }
-
   });
-
-  // swiperNews.on('slideChange', () => {
-  //   console.log('slider change');
-  //   console.log(swiperNews.activeIndex);
-  // });
 };
 
 export { initSwiperNews };
